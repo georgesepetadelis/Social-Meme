fc7523cd57d7c912f8ed96ec9af1982d346d3c2f
georgesepetadelis <giorgossepetadelis11@gmail.com>
2022-03-23 22:21:44 +0200

    Update username requirements to 5-13 characters

--- app/src/main/java/com/george/socialmeme/Activities/HomeActivity.java
+++ app/src/main/java/com/george/socialmeme/Activities/HomeActivity.java
@@ -3,7 +3,6 @@ package com.george.socialmeme.Activities;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -16,29 +15,17 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import com.george.socialmeme.Fragments.HomeFragment;
 import com.george.socialmeme.Fragments.MyProfileFragment;
 import com.george.socialmeme.Fragments.NewPostFragment;
-import com.george.socialmeme.Models.PostModel;
 import com.george.socialmeme.R;
-import com.george.socialmeme.Receivers.DailyNotificationReceiver;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
-
-import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -49,28 +36,6 @@ public class HomeActivity extends AppCompatActivity {
     boolean isNightModeEnabled() {
         SharedPreferences sharedPref = getSharedPreferences("dark_mode", MODE_PRIVATE);
         return sharedPref.getBoolean("dark_mode", false);
-    }
-
-    private void registerDailyNotification() {
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 18);
-        calendar.set(Calendar.MINUTE, 30);
-        calendar.set(Calendar.SECOND, 0);
-
-        Intent intent = new Intent(getApplicationContext(), DailyNotificationReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 100,
-                intent, PendingIntent.FLAG_IMMUTABLE);
-
-        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-                AlarmManager.INTERVAL_DAY, pendingIntent);
-
-        SharedPreferences sharedPref = getSharedPreferences("daily_notification_registered", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putBoolean("daily_notification_registered", true);
-        editor.apply();
-
     }
 
     void enableNightMode() {
@@ -99,14 +64,6 @@ public class HomeActivity extends AppCompatActivity {
 
         final FirebaseAuth mAuth = FirebaseAuth.getInstance();
         bottomNavBar = findViewById(R.id.bottom_nav);
-
-        // Check if daily notification broadcast receiver has been registered
-        SharedPreferences sharedPref = getSharedPreferences("daily_notification_registered", MODE_PRIVATE);
-        boolean notificationBroadcastRegistered = sharedPref.getBoolean("daily_notification_registered", false);
-
-        if (!notificationBroadcastRegistered) {
-            registerDailyNotification();
-        }
 
         // Detect if system night mode is enabled
         // to auto enable in-app night mode
